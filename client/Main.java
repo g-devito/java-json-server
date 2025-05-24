1cedc45ed90765a1382e952aa5647f9ed1f9b36c
giochac <giodevito99@gmail.com>
2025-05-24 18:39:43 +0200

    create client/Main.java basic functionalities: set, get, delete

--- client/Main.java
+++ client/Main.java
@@ -1,7 +1,69 @@
 package client;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Client!");
+        String[] database = new String[1000];
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine().trim();
+            int index;
+            String value;
+
+            if (input.isEmpty()) continue;
+
+            String[] inputArray = input.split(" ", 3);
+            String verb = inputArray[0];
+
+            switch (verb) {
+                case "set":
+                    if (inputArray.length != 3) {
+                        System.out.println("ERROR");
+                        continue;
+                    }
+                    index = Integer.parseInt(inputArray[1]);
+                    value = inputArray[2];
+                    if (index > 1000)
+                        System.out.println("ERROR");
+                    else {
+                        database[index] = value;
+                        System.out.println("OK");
+                    }
+                    break;
+                case "get":
+                    if (inputArray.length != 2) {
+                        System.out.println("ERROR");
+                        continue;
+                    }
+                    index = Integer.parseInt(inputArray[1]);
+                    if (index > 1000 || database[index] == null)
+                        System.out.println("ERROR");
+                    else
+                        System.out.println(database[index]);
+                    break;
+                case "delete":
+                    if (inputArray.length != 2) {
+                        System.out.println("ERROR");
+                        continue;
+                    }
+                    index = Integer.parseInt(inputArray[1]);
+                    if (index > 1000)
+                        System.out.println("ERROR");
+                    else {
+                        database[index] = null;
+                        System.out.println("OK");
+                    }
+                    break;
+                case "exit":
+                    if (inputArray.length != 1) {
+                        System.out.println("ERROR");
+                        continue;
+                    }
+                    return;
+                default:
+                    System.out.printf("wrong verb: %s%n", verb);
+            }
+        }
     }
 }
