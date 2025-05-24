c13d45125843861590e7a34255ec5378ae611780
giochac <giodevito99@gmail.com>
2025-05-24 17:09:41 +0200

    first commit

--- client/Main.java
+++ client/Main.java
@@ -2,6 +2,6 @@ package client;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, client!");
+        System.out.println("Hello, Client!");
     }
 }
