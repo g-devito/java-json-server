7d7ebf36871a033765dff6e845d6f7fd74206bda
giochac <giodevito99@gmail.com>
2025-05-24 19:19:56 +0200

    test socket client, server communication via client/Main.java and server/Main.java

--- client/Main.java
+++ client/Main.java
@@ -1,69 +1,90 @@
 package client;
 
-import java.util.Scanner;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+//import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] database = new String[1000];
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String input = scanner.nextLine().trim();
-            int index;
-            String value;
-
-            if (input.isEmpty()) continue;
-
-            String[] inputArray = input.split(" ", 3);
-            String verb = inputArray[0];
-
-            switch (verb) {
-                case "set":
-                    if (inputArray.length != 3) {
-                        System.out.println("ERROR");
-                        continue;
-                    }
-                    index = Integer.parseInt(inputArray[1]);
-                    value = inputArray[2];
-                    if (index > 1000)
-                        System.out.println("ERROR");
-                    else {
-                        database[index] = value;
-                        System.out.println("OK");
-                    }
-                    break;
-                case "get":
-                    if (inputArray.length != 2) {
-                        System.out.println("ERROR");
-                        continue;
-                    }
-                    index = Integer.parseInt(inputArray[1]);
-                    if (index > 1000 || database[index] == null)
-                        System.out.println("ERROR");
-                    else
-                        System.out.println(database[index]);
-                    break;
-                case "delete":
-                    if (inputArray.length != 2) {
-                        System.out.println("ERROR");
-                        continue;
-                    }
-                    index = Integer.parseInt(inputArray[1]);
-                    if (index > 1000)
-                        System.out.println("ERROR");
-                    else {
-                        database[index] = null;
-                        System.out.println("OK");
-                    }
-                    break;
-                case "exit":
-                    if (inputArray.length != 1) {
-                        System.out.println("ERROR");
-                        continue;
-                    }
-                    return;
-                default:
-                    System.out.printf("wrong verb: %s%n", verb);
-            }
+//        String[] database = new String[1000];
+//        Scanner scanner = new Scanner(System.in);
+        String address = "127.0.0.1";
+        int port = 23456;
+        try (
+                Socket socket = new Socket(InetAddress.getByName(address), port);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+        ) {
+            String msg = "Give me a record # 12";
+            System.out.println("Client started!");
+            output.writeUTF(msg);
+            System.out.printf("Sent: %s%n", msg);
+            System.out.printf("Received: %s%n", input.readUTF());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+//        while (true) {
+//            String input = scanner.nextLine().trim();
+//            int index;
+//            String value;
+//
+//            if (input.isEmpty()) continue;
+//
+//            String[] inputArray = input.split(" ", 3);
+//            String verb = inputArray[0];
+//
+//            switch (verb) {
+//                case "set":
+//                    if (inputArray.length != 3) {
+//                        System.out.println("ERROR");
+//                        continue;
+//                    }
+//                    index = Integer.parseInt(inputArray[1]);
+//                    value = inputArray[2];
+//                    if (index > 1000)
+//                        System.out.println("ERROR");
+//                    else {
+//                        database[index] = value;
+//                        System.out.println("OK");
+//                    }
+//                    break;
+//                case "get":
+//                    if (inputArray.length != 2) {
+//                        System.out.println("ERROR");
+//                        continue;
+//                    }
+//                    index = Integer.parseInt(inputArray[1]);
+//                    if (index > 1000 || database[index] == null)
+//                        System.out.println("ERROR");
+//                    else
+//                        System.out.println(database[index]);
+//                    break;
+//                case "delete":
+//                    if (inputArray.length != 2) {
+//                        System.out.println("ERROR");
+//                        continue;
+//                    }
+//                    index = Integer.parseInt(inputArray[1]);
+//                    if (index > 1000)
+//                        System.out.println("ERROR");
+//                    else {
+//                        database[index] = null;
+//                        System.out.println("OK");
+//                    }
+//                    break;
+//                case "exit":
+//                    if (inputArray.length != 1) {
+//                        System.out.println("ERROR");
+//                        continue;
+//                    }
+//                    return;
+//                default:
+//                    System.out.printf("wrong verb: %s%n", verb);
+//            }
+//        }
     }
 }
--- server/Main.java
+++ server/Main.java
@@ -1,8 +1,36 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
+        String address = "127.0.0.1";
+        int port = 23456;
 
+        try (ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address))) {
+            System.out.println("Server started, waiting for client...");
+            try (Socket socket = server.accept();
+                 DataInputStream input = new DataInputStream(socket.getInputStream());
+                 DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+
+                System.out.println("Client connected!");
+
+                String receivedMsg = input.readUTF();
+                System.out.printf("Received: %s%n", receivedMsg);
+
+                String responseMsg = "A record # 12 was sent!";
+                System.out.printf("Sent: %s%n", responseMsg);
+
+                output.writeUTF(responseMsg);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
